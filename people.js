09e6ec4cd2ac7b1e8ed9aeed0406323ddf321ade
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-14 23:13:26 +0200

    [UPD]Add-people-require

--- people.js
+++ people.js
@@ -2,3 +2,24 @@
 //1. Importa la tua funzione da names.js
 //2. Importa la tua funzione da hobbies.js
 //3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+
+
+
+
+
+
+// Importa la tua funzione da names.js
+const creaNome = require('./name.js');
+// Importa la tua funzione da hobbies.js
+const creaHobby = require('./hobbies.js');
+
+//creo la funzione senza paramerri
+
+function creaPersona() {
+
+    return{
+        
+    }
+
+
+}
