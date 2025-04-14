0a6f32b7de21549b08a5dd2bebcd5df99c5a1ae2
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-14 23:03:59 +0200

    [UPD]Add-name.js

--- name.js
+++ name.js
@@ -1,2 +1,12 @@
 //1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 //2. Esporta la funzione dal file.
+
+function creaNome(firstName, lastName){
+    return{
+        firstName: Mario,
+        lastName: Rossi
+    }
+}
+//questa serve per esportare la funzione in modo che possa essere utilizzata in altri file
+//per esempio in people.js
+module.exports = creaNome;
