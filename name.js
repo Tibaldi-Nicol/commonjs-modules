7bfcbba3c98cbdb76637efe8aac3ade7a964f248
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-14 23:00:16 +0200

    first commit

---
+++ name.js
@@ -0,0 +1,2 @@
+//1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+//2. Esporta la funzione dal file.
